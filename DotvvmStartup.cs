673e553daac697fd085506f17373459380c3772b
VineethaGopisetty <vineetha_gopisetty@persistent.com>
2022-04-28 16:55:15 +0530

    Add project files.

---
+++ DotvvmStartup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DotVVM.Framework.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotvvmApp1
+{
+    public class DotvvmStartup : IDotvvmStartup, IDotvvmServiceConfigurator
+    {
+        public void Configure(DotvvmConfiguration config, string applicationPath)
+        {
+            ConfigureRoutes(config,applicationPath);
+            ConfigureResources(config, applicationPath);
+            ConfigureControls(config, applicationPath);
+        }
+
+        private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
+        {
+          
+        }
+
+        private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
+        {
+
+        }
+
+        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
+        {
+            config.RouteTable.Add("About","","Views/About.dothtml");
+            config.RouteTable.Add("Students", "students", "Views/Students.dothtml");
+        }
+
+        public void ConfigureServices(IDotvvmServiceCollection options)
+        {
+            
+        }
+    }
+}
